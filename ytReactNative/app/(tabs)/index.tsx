44c67f597394deed5d254d0d57af4adfc40fd896
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-28 19:10:40 +0530

    Updated the code in this part

--- ytReactNative/app/(tabs)/index.tsx
+++ ytReactNative/app/(tabs)/index.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { red } from "react-native-reanimated/lib/typescript/Colors";
 
 const Index = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.whiteText}>Hello</Text>
+      <Text style={styles.whiteText}>HELLO THIS IS HARSH AND I LOVE CODING</Text>
+      <Image />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,               // take full screen
+    flex: 1,                // take full screen
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#000", // optional: black background to see white text
+    backgroundColor: "black", // black background
   },
   whiteText: {
-    color: "#fff", // white color
-    fontSize: 20,  // optional: increase size
+    color: "white", // white text
+    fontSize: 20,  // bigger text
   },
 });
 
