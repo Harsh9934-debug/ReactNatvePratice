4bb1cca98b9ca9e50e360c553789c2013a68c216
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-28 19:16:12 +0530

    In this part i learned that how to add the image

--- ytReactNative/app/(tabs)/index.tsx
+++ ytReactNative/app/(tabs)/index.tsx
@@ -6,7 +6,10 @@ const Index = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.whiteText}>HELLO THIS IS HARSH AND I LOVE CODING</Text>
-      <Image />
+      <Image
+        style = {{width:200 , height:400}}
+        source={{uri:"https://images.unsplash.com/photo-1756370473190-4c41ddbd5e59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"}}
+      />
     </View>
   );
 };
