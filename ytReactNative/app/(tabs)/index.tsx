a7120907ca24452e6e7cf8b73bf1c4de476459d9
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-09-01 01:49:00 +0530

    updated the scrollView property

--- ytReactNative/app/(tabs)/index.tsx
+++ ytReactNative/app/(tabs)/index.tsx
@@ -5,19 +5,19 @@ const Index = () => {
     <View style={styles.container}>
       <Text style={styles.whiteText}>HELLO THIS IS HARSH </Text>
       <Image
-        style = {{width:200 , height:400}}
-        source={{uri:"https://images.unsplash.com/photo-1756370473190-4c41ddbd5e59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"}}
+        style={{ width: 200, height: 400 }}
+        source={{ uri: "https://images.unsplash.com/photo-1756370473190-4c41ddbd5e59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8" }}
       />
 
-      <Button title ='click me' onPress={()=>{alert("Button is clicked")}}></Button> 
+      <Button title='click me' onPress={() => { alert("Button is clicked") }}></Button>
       {/* this Button property is no that good this is haviing the prebuilt styling */}
 
       <TouchableOpacity>
-        <Text style= {styles.whiteText2} onPress={()=>{{Alert.alert("This is from the touchable opacity")}}}>this is from the different button</Text>
+        <Text style={styles.whiteText2} onPress={() => { { Alert.alert("This is from the touchable opacity") } }}>this is from the different button</Text>
       </TouchableOpacity>
 
       <Pressable>
-        <Text style={styles.whiteText3} onPress={()=>{Alert.alert("This is a sample message")}}>Press me </Text>
+        <Text style={styles.whiteText3} onPress={() => { Alert.alert("This is a sample message") }}>Press me </Text>
       </Pressable>
     </View>
   );
@@ -25,10 +25,10 @@ const Index = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,                
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "black", 
+    backgroundColor: "black",
   },
   whiteText: {
     color: "white", // white text
@@ -37,15 +37,15 @@ const styles = StyleSheet.create({
   whiteText2: {
     color: "white", // white text
     fontSize: 20,
-    padding:20, 
-    margin:50,  
-    backgroundColor:"red"
+    padding: 20,
+    margin: 50,
+    backgroundColor: "red"
   },
   whiteText3: {
     color: "white", // white text
     fontSize: 20,
-    padding:20, 
-    backgroundColor:"purple"
+    padding: 20,
+    backgroundColor: "purple"
   },
 });
 
